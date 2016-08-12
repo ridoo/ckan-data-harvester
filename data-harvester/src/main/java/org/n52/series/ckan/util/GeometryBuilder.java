2398d41b3f01367d6fce4466be819376158fc4a8
Henning Bredel <bredel@52north.org>
2016-08-12 10:08:34 +0200

    add convenience check

--- data-harvester/src/main/java/org/n52/series/ckan/util/GeometryBuilder.java
+++ data-harvester/src/main/java/org/n52/series/ckan/util/GeometryBuilder.java
@@ -91,13 +91,17 @@ public class GeometryBuilder {
         namedValue.setValue(new GeometryValue(getGeometry()));
         return namedValue;
     }
+    
+    public boolean canBuildGeometry() {
+        return geometry != null || hasCoordinates();
+    }
 
     public Geometry getGeometry() {
+        if ( !canBuildGeometry()) {
+            return null;
+        }
         if (geometry != null) {
             return geometry;
-        }
-        if ( !hasCoordinates()) {
-            return null;
         }
         final Point lonLatPoint = utils.createPoint(longitude, latitude, altitude, crs);
         try {
