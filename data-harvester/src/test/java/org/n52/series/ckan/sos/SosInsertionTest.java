f5ea30e7b91f3b7d751b10b0fa37b07c13e4b6ac
Henning Bredel <bredel@52north.org>
2016-12-08 11:05:33 +0100

    reduce test datasets ..
    .. run each test case individually, if inserts of even reduced
    datasets are too slow

--- data-harvester/src/test/java/org/n52/series/ckan/sos/SosInsertionTest.java
+++ data-harvester/src/test/java/org/n52/series/ckan/sos/SosInsertionTest.java
@@ -47,6 +47,7 @@ import org.n52.sos.ds.hibernate.HibernateTestCase;
 import org.n52.sos.exception.ConfigurationException;
 import org.n52.sos.ogc.ows.OwsExceptionReport;
 
+@Ignore("currently toooooo slooooooooooow for unit testing")
 public class SosInsertionTest extends HibernateTestCase {
 
     private FileBasedCkanHarvestingService service;
@@ -60,63 +61,54 @@ public class SosInsertionTest extends HibernateTestCase {
     }
 
     @Test
-    @Ignore("currently toooooo slooooooooooow for unit testing")
     public void when_inserting_DWDWind_dataset_then_getObservationNotEmpty()
             throws OwsExceptionReport, IOException, URISyntaxException {
         assertDataInsertion("12c3c8f1-bf44-47e1-b294-b6fc889873fc");
     }
 
     @Test
-    @Ignore("currently toooooo slooooooooooow for unit testing")
     public void when_inserting_DWDTemperature_dataset_then_getObservationNotEmpty()
             throws OwsExceptionReport, IOException, URISyntaxException {
         assertDataInsertion("eab53bfe-fce7-4fd8-8325-a0fe5cdb23c8");
     }
 
     @Test
-    @Ignore("currently toooooo slooooooooooow for unit testing")
     public void when_inserting_DWDSun_dataset_then_getObservationNotEmpty()
             throws OwsExceptionReport, IOException, URISyntaxException {
         assertDataInsertion("582ca1ba-bdc0-48de-a685-3184339d29f0");
     }
 
     @Test
-    @Ignore("currently toooooo slooooooooooow for unit testing")
     public void when_inserting_DWDPrecipitation_dataset_then_getObservationNotEmpty()
             throws OwsExceptionReport, IOException, URISyntaxException {
         assertDataInsertion("26128007-1fa7-475f-a4f1-5e798185eab9");
     }
 
     @Test
-    @Ignore("currently toooooo slooooooooooow for unit testing")
     public void when_inserting_FraunhoferIgdTemp_dataset_then_getObservationNotEmpty()
             throws OwsExceptionReport, IOException, URISyntaxException {
         assertDataInsertion("f5c3eb65-c695-49a4-8992-ed54f973b950");
     }
 
     @Test
-    @Ignore("currently toooooo slooooooooooow for unit testing")
     public void when_inserting_FraunhoferIgdCoord_dataset_then_getObservationNotEmpty()
             throws OwsExceptionReport, IOException, URISyntaxException {
         assertDataInsertion("3aa04d90-4003-408a-8f3f-463dd6cb7486");
     }
 
     @Test
-    @Ignore("currently toooooo slooooooooooow for unit testing")
     public void when_inserting_OpenWeatherMapCoord_dataset_then_getObservationNotEmpty()
             throws OwsExceptionReport, IOException, URISyntaxException {
         assertDataInsertion("fb0f0f57-7a01-4385-9fe1-9fb366a63c4e");
     }
 
     @Test
-    @Ignore("currently toooooo slooooooooooow for unit testing")
     public void when_inserting_OpenWeatherMapTemp_dataset_then_getObservationNotEmpty()
             throws OwsExceptionReport, IOException, URISyntaxException {
         assertDataInsertion("a5442a6a-0a84-4326-a5b5-e6288e8fa457");
     }
 
     @Test
-//    @Ignore("currently toooooo slooooooooooow for unit testing")
     public void when_inserting_heavyMetalSamples_dataset_then_getObservationNotEmpty()
             throws OwsExceptionReport, IOException, URISyntaxException {
         assertDataInsertion("3eb54ee2-6ec5-4ad9-af96-264159008aa7");
