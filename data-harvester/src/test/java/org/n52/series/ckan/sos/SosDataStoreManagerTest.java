b4c08c2f05cd27da10c26a7e7b276f9960852e63
Henning Bredel <bredel@52north.org>
2017-03-28 15:34:06 +0200

    ignore dataset test which needs more discussion

--- data-harvester/src/test/java/org/n52/series/ckan/sos/SosDataStoreManagerTest.java
+++ data-harvester/src/test/java/org/n52/series/ckan/sos/SosDataStoreManagerTest.java
@@ -225,7 +225,7 @@ public class SosDataStoreManagerTest extends HibernateTestCase {
     }
 
     @Test
-//    @Ignore("this dataset needs some discussion first")
+    @Ignore("this dataset needs some discussion first")
     public void when_inserting_emmissionSimulation_dataset_then_getObservationNotEmpty() {
         // TODO simulation results need discussion
         // 1) a mobile platform (car) which is declared as observation --> observation_with_geometry
