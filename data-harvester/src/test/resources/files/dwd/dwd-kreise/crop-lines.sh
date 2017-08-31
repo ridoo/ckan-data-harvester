f8f2b26c313cd5ae3ce61d6e8416b2685a050619
Henning Bredel <h.bredel@52north.org>
2017-08-31 14:39:00 +0200

    update test data

--- data-harvester/src/test/resources/files/dwd/dwd-kreise/crop-lines.sh
+++ data-harvester/src/test/resources/files/dwd/dwd-kreise/crop-lines.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 
 LINE_RANGE="1,10p"
-IGNORE_FILES=(
+declare -a IGNORE_FILES=(
     # represents districts
-    "ebe26100-ef92-4b84-8e9e-7ce3a1f7d44e.csv"
+    "./ebe26100-ef92-4b84-8e9e-7ce3a1f7d44e.csv"
+    "./546add17-3bcc-46a8-a550-a9e2e07a4648.csv"
 )
 
 # TODO 
 # make ignores to array: -not \( -path './etc/*' -o -path './cache/*' -o -path './archive/*'  \)
 
-add_excludes() {
-    declare -a ignore_files=("${!1}")
-    if [ ${ignore_files[@]} -eq 0 ]; then
-        return "";
-    else
-        ignore=
-        for i in "${ignore_files[@]}"
-        do
-           echo $i
-        done
-        return "$ignore \)"
+function add_excludes {
+    declare -a items=("$@")
+    
+    if [ "${#items[*]}" -eq 0 ]; then
+        echo ""
     fi
+
+    local excludes="-not \( -path \"${items[0]}\""
+    if [ "${#items[*]}" -gt 1 ]; then
+        for i in "${items[@]:1}"
+        do
+           excludes="$excludes -o -path \"${i}\""
+        done
+    fi
+
+    echo "$excludes \)"
 }
 
 # inplace cropping lines of given range within CSV files in current folder
-for file in `find . -type f -name '*.csv' -not \( -path "$IGNORE_FILES[0]" \)` ; do sed -i -n $LINE_RANGE "$file" ; done
+exclude_files=$(add_excludes "${IGNORE_FILES[@]}")
+echo "$exclude_files"
+echo find . -type f -name \"*.csv\" "$exclude_files" -exec sed -i -n \"$LINE_RANGE\" {} \\\;
+find . -type f -name \"*.csv\" "$exclude_files" -exec sed -i -n \"$LINE_RANGE\" {} \\\;
+#find . -name "*.csv" -not \( -path "ebe*" -o -path "546add*" \) -exec sed -i -n "1,10p" {} \;
+
 
 #for file in `find . -type f -name '*.csv add_excludes IGNORE_FILES[@]'` ; do sed -i -n $LINE_RANGE "$file" ; done
 
