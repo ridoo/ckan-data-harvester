23f35ecef8e9b3f69bb5ff72e565e34393141b06
Henning Bredel <h.bredel@52north.org>
2017-09-07 15:52:11 +0200

    update sandbox testing loading csv->table

--- data-harvester/src/test/java/org/n52/series/ckan/tool/CsvResourceReaderTest.java
+++ data-harvester/src/test/java/org/n52/series/ckan/tool/CsvResourceReaderTest.java
@@ -1,7 +1,6 @@
 
 package org.n52.series.ckan.tool;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -12,6 +11,7 @@ import java.util.function.BiFunction;
 import java.util.regex.Pattern;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -29,6 +29,7 @@ import org.n52.series.ckan.util.TestConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Ignore("sandbox to try out alternate csv csv->table loading strategies")
 public class CsvResourceReaderTest {
 
     private static final String DWD_LARGE_RESOURCE_MEMBER = "b5b7e5cb-25c7-46e8-b6e5-22521cfc9a97";
@@ -40,42 +41,57 @@ public class CsvResourceReaderTest {
     @Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
 
-    private ResourceTestHelper testHelper;
+    private DataCollection dataCollection;
 
     @Before
     public void setUp() throws URISyntaxException, IOException {
-        String trimmedData = "/files/" + TestConstants.TEST_COMPLETE_DATA_FOLDER;
-        testHelper = new ResourceTestHelper(testFolder, trimmedData);
+        // String dataFolder = "/files/" + TestConstants.TEST_TRIMMED_DATA_FOLDER;
+        String dataFolder = "/files/" + TestConstants.TEST_TRIMMED_DATA_FOLDER;
+        ResourceTestHelper testHelper = new ResourceTestHelper(testFolder, dataFolder);
+        dataCollection = testHelper.getDataCollection(DWD_KREISE_DATASET_ID);
     }
 
     @Test
+    @Ignore
     public void readViaDefaultCsvReader() {
-//         readResourceTable(CsvTableLoader::new);
-        readResourceTable(CustomCsvTableLoader::new);
-    }
-
-    private void readResourceTable(BiFunction<ResourceTable, DataFile, TableLoader> tableLoaderSupplier) {
-        DataCollection dataCollection = testHelper.getDataCollection(DWD_KREISE_DATASET_ID);
-        Entry<ResourceMember, DataFile> entry = dataCollection.getDataEntry(DWD_LARGE_RESOURCE_MEMBER);
-        readData(new ResourceTable(entry) {
-            @Override
-            protected TableLoader createTableLoader(DataFile file) {
-                return tableLoaderSupplier.apply(this, entry.getValue());
-            }
-        });
-    }
-
-    private void readData(ResourceTable table) {
         long start = System.currentTimeMillis();
-        table.readIntoMemory();
+        Entry<ResourceMember, DataFile> largeMember = dataCollection.getDataEntry(DWD_LARGE_RESOURCE_MEMBER);
+//        readResourceTable(BufferedReaderForLoopLoader::new, largeMember);
+         readResourceTable(BufferedReaderStreamLoader::new, largeMember);
+//         readResourceTable(CsvTableLoader::new, largeMember);
         LOGGER.info("Reading to memory took {}s", (System.currentTimeMillis() - start) / 1000d);
     }
 
-    private static class CustomCsvTableLoader extends TableLoader {
+    @Test
+    public void readAllDataReader() {
+        long start = System.currentTimeMillis();
+//         readWholeDataCollection(CsvTableLoader::new); // 1.8s
+        readWholeDataCollection(BufferedReaderStreamLoader::new); // 1.8s
+//        readWholeDataCollection(BufferedReaderForLoopLoader::new); // 2.0s
+        LOGGER.info("Reading to memory took {}s", (System.currentTimeMillis() - start) / 1000d);
+    }
+
+    private void readWholeDataCollection(BiFunction<ResourceTable, DataFile, TableLoader> tableLoader) {
+        for (Entry<ResourceMember, DataFile> entry : dataCollection) {
+            readResourceTable(tableLoader, entry);
+        }
+    }
+
+    private void readResourceTable(BiFunction<ResourceTable, DataFile, TableLoader> tableLoader,
+                                   Entry<ResourceMember, DataFile> entry) {
+        new ResourceTable(entry) {
+            @Override
+            protected TableLoader createTableLoader(DataFile file) {
+                return tableLoader.apply(this, entry.getValue());
+            }
+        }.readIntoMemory();
+    }
+
+    private static class BufferedReaderForLoopLoader extends TableLoader {
 
         private int lineNbr;
-        
-        public CustomCsvTableLoader(DataTable table, DataFile datafile) {
+
+        public BufferedReaderForLoopLoader(DataTable table, DataFile datafile) {
             super(table, datafile);
         }
 
@@ -90,18 +106,54 @@ public class CsvResourceReaderTest {
             List<String> columnHeaders = resourceMember.getColumnHeaders();
             int headerRows = resourceMember.getHeaderRows();
             try {
-                BufferedReader reader = Files.newBufferedReader(file.toPath(), dataFile.getEncoding());
-                reader.lines()
+                List<String> allLines = Files.readAllLines(file.toPath());
+                for (int i = headerRows; i < allLines.size(); i++) {
+                    ResourceKey id = resourceMember.createResourceKey(lineNbr++);
+                    String[] values = csvValuesPatter.split(allLines.get(i));
+                    for (int j = 0; j < columnHeaders.size(); j++) {
+                        final ResourceField field = resourceMember.getField(j);
+                        setCellValue(id, field, values[j]);
+                    }
+                }
+            } catch (IOException e) {
+                throw new TableLoadException("Unable to read file: " + file, e);
+            }
+
+        }
+
+    }
+
+    private static class BufferedReaderStreamLoader extends TableLoader {
+
+        private int lineNbr;
+
+        public BufferedReaderStreamLoader(DataTable table, DataFile datafile) {
+            super(table, datafile);
+        }
+
+        @Override
+        public void loadData() throws TableLoadException {
+            DataFile dataFile = getDataFile();
+            File file = dataFile.getFile();
+            ResourceMember resourceMember = getResourceMember();
+
+            lineNbr = 0;
+            Pattern csvValuesPatter = Pattern.compile(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+            List<String> columnHeaders = resourceMember.getColumnHeaders();
+            int headerRows = resourceMember.getHeaderRows();
+            try {
+                Files.newBufferedReader(file.toPath(), dataFile.getEncoding())
+                     .lines()
                      .skip(headerRows)
                      .forEach(l -> {
                          ResourceKey id = resourceMember.createResourceKey(lineNbr++);
-//                         String[] values = l.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+                         // String[] values = l.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
                          String[] values = csvValuesPatter.split(l);
                          for (int j = 0; j < columnHeaders.size(); j++) {
                              final ResourceField field = resourceMember.getField(j);
                              setCellValue(id, field, values[j]);
                          }
-                         
+
                      });
             } catch (IOException e) {
                 throw new TableLoadException("Unable to read file: " + file, e);
